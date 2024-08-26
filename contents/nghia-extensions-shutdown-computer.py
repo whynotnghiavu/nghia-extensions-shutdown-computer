616c77820d2618097ad71fe04e07c391568a0e23
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-26 15:17:50 +0700

    update nghia-extensions-shutdown-computer.py

---
+++ contents/nghia-extensions-shutdown-computer.py
@@ -0,0 +1,18 @@
+import os 
+
+
+
+
+
+
+def shutdown_computer():
+    if os.name == 'nt':
+        os.system('shutdown /s /t 15')
+    elif os.name == 'posix':
+        time.sleep(15)
+        os.system('shutdown now')
+    else:
+        print("Không thể tắt máy tự động trên hệ điều hành này.")
+
+ 
+shutdown_computer()
